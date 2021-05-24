5c8e9a57f2307dba03b19d972b155968ad5e5be0
eslam ahmed ali hassan <eslam.pop911@yahoo.com>
2021-05-24 15:59:16 +0200

    Add project files.

---
+++ CommunicatingBetweenControls/Mediator.cs
@@ -0,0 +1,31 @@
+﻿using CommunicatingBetweenControls.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicatingBetweenControls
+{
+    public sealed class Mediator
+    {
+        //Singleton functionality
+        private static readonly Mediator Instance = new Mediator();
+        
+
+        public static Mediator GetInstance()
+        {
+            return Instance;
+        }
+
+        //Instance functionality
+        public event EventHandler<JobChangedEventArgs> JobChanged;
+
+        public void OnJobChanged(Object sender, Job job)
+        {
+            var jobChangeDelegate = JobChanged;
+            jobChangeDelegate?.Invoke(sender, new JobChangedEventArgs { Job = job });
+        }
+
+    }
+}
